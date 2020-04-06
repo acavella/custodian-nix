1c3670f17e6a2fd8a07ac2e2ace6a822f7bf0402
Tony Cavella <acavella@gmail.com>
2020-04-06 13:02:21 -0400

    Add OpenSSL CSR

---
+++ custodian.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# SET DEBUG
+set -x
+
+# GLOBAL VARIABLES
+BIN_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
+LIB_PATH="$BIN_PATH/lib/"
+WRK_PATH="$( pwd )"
+
+# LOAD LIBRARIES
+source $LIB_PATH/functions.sh
+source $LIB_PATH/help.sh
+
+if [[ "$1" == "CSR" ]];
+then
+	generate_csr
+elif [[ "$1" == "--help" ]];
+then
+	display_help
+elif [[ -z $1 ]];
+then 
+	display_help
+fi
