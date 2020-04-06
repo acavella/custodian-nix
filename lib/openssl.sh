24bfce621bf7baf450f9987d0ba0acded1a3635e
Tony Cavella <acavella@gmail.com>
2020-04-06 14:48:05 -0400

    Update openssl function

--- lib/openssl.sh
+++ lib/openssl.sh
@@ -1,9 +1,12 @@
 generate_csr () {
+	# Assign serial number variable
 	read -p "Device serial number: " serialNum
+
+	# Generate private key and signing request
 	echo "Generating ECDSA private key for $serialNum"
 	cp $BIN_PATH/conf/dmccts.conf /tmp/$serialNum.conf
 	echo "CN=$serialNum" >> /tmp/$serialNum.conf
 	/usr/bin/openssl ecparam -name secp384r1 -genkey -noout -out $serialNum.key
-	/usr/bin/openssl req -new -config $BIN_PATH/conf/$serialNum.conf -sha384 -key $serialNum.key -out $serialNum.csr
+	/usr/bin/openssl req -new -config /tmp/$serialNum.conf -sha384 -key $serialNum.key -out $serialNum.csr
 	rm -f /tmp/$serialNum.conf
 }
