6dcbf51fe713458d514b20049ff19107e7f7a438
Tony Cavella <acavella@gmail.com>
2020-04-06 13:29:02 -0400

    Update OpenSSL temporary file handling.

--- lib/openssl.sh
+++ lib/openssl.sh
@@ -1,9 +1,9 @@
 generate_csr () {
 	read -p "Device serial number: " serialNum
 	echo "Generating ECDSA private key for $serialNum"
-	cp $BIN_PATH/conf/dmccts.conf $BIN_PATH/conf/$serialNum.conf
-	echo "CN=$serialNum" >> $BIN_PATH/conf/$serialNum.conf
+	cp $BIN_PATH/conf/dmccts.conf /tmp/$serialNum.conf
+	echo "CN=$serialNum" >> /tmp/$serialNum.conf
 	/usr/bin/openssl ecparam -name secp384r1 -genkey -noout -out $serialNum.key
 	/usr/bin/openssl req -new -config $BIN_PATH/conf/$serialNum.conf -sha384 -key $serialNum.key -out $serialNum.csr
-	rm -f $BIN_PATH/conf/$serialNum.conf
+	rm -f /tmp/$serialNum.conf
 }
