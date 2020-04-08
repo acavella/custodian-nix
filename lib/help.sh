aaf2171574aaf34b9b6b506662f2dd053b0ca1c8
Tony Cavella <acavella@gmail.com>
2020-04-08 14:18:17 -0400

    Update help context

--- lib/help.sh
+++ lib/help.sh
@@ -1,22 +1,21 @@
 display_help () {
 echo "
-cd: cd [-L|[-P [-e]] [-@]] [dir]
-    Change the shell working directory.
-    
-    Change the current directory to DIR.  The default DIR is the value of the
-    HOME shell variable.
+custudian.sh: ./custodian.sh [function] [args]
+    Assists with common administrative activities.
     
     The variable CDPATH defines the search path for the directory containing
     DIR.  Alternative directory names in CDPATH are separated by a colon (:).
     A null directory name is the same as the current directory.  If DIR begins
     with a slash (/), then CDPATH is not used.
     
-    If the directory is not found, and the shell option `cdable_vars' is set,
-    the word is assumed to be  a variable name.  If that variable has a value,
-    its value is used for DIR.
-    
-    Options:
-      -L	force symbolic links to be followed: resolve symbolic
-    		links in DIR after processing instances of `..'
+    Functions:
+      csr	Generates a private key and certificate signing request.
+      		Argument specifies CN value, full RDN defined in 
+		conf/openssl.conf
+		Example: custodian.sh csr YOUR_CN_HERE
+
+      bulkcsr	Generates multiple private keys and certificate signing requests.
+      		Argument specifies txt file containing list of CN values.
+		Example: custodian.sh bulkcsr path/cnlist.txt
 "
 }
